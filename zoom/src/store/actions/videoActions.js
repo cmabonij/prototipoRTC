29ce16c60f3977bbcf24c9ee6d44dbd9b1faf555
reubber de sousa <reubbers@gmail.com>
2022-10-17 11:33:15 -0300

    fix: setup web-rtc

--- zoom/src/store/actions/videoActions.js
+++ zoom/src/store/actions/videoActions.js
@@ -1,11 +1,10 @@
 import IO from 'socket.io-client';
-import peer from 'react-native-peerjs';
+import Peer from 'react-native-peerjs';
 
-import { ADD_REMOTE_STREAM, MY_STREAM, ADD_STREAM, JOIN_CHAT } from './types';
-import Peer from 'peerjs';
+import {ADD_REMOTE_STREAM, MY_STREAM, ADD_STREAM, JOIN_CHAT} from './types';
 
 //*API URL*/
-export const API_URI = `http://192.168.16.119:5000`;
+export const API_URI = 'http://192.168.16.119:5000';
 
 //* Peer Config*/
 const peerServer = new Peer(undefined, {
@@ -18,28 +17,29 @@ const peerServer = new Peer(undefined, {
 //*Socket Config */
 export const socket = IO(`${API_URI}`, {
   forceNew: true,
-})
+});
 
 socket.on('connection', () => console.log('Connected client'));
 
 export const jointRoom = (stream) => async (dispatch) => {
-  const roomID = "aaabbb";
+  const roomID = 'aaabbb';
   //set my own stream
-  dispatch({ type: MY_STREAM, payload: stream }); r
+  dispatch({type: MY_STREAM, payload: stream});
+  r;
 
   //open connection
   peerServer.on('open', (userId) => {
-    socket.emit('join-room', { userId, roomID });
+    socket.emit('join-room', {userId, roomID});
   });
-  /* 
+  /*
     socket.on('user-connected', (userId) => {
       connectToNewUser(userId, stream, dispatch);
     });
-  
+
     // receive the call
     peerServer.on('call', (call) => {
       call.answer(stream);
-  
+
       // strean back the call
       call.on('stream', (stream) => {
         dispatch({ type: ADD_STREAM, payload: stream });
